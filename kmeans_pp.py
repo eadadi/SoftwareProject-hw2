f53fff4153372264c06493ab7971e1b48ef329d1
YonatanEr <yonatan02111995@gmail.com>
2021-06-12 16:20:41 +0300

    Probably Working
    הוספתי הדפסה על פי פורמט ופקודות הרצה זריזות

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -7,10 +7,10 @@ C Stuff
 '''
 import mykmeanssp as km
 
-
 '''
 The code:
 '''
+
 
 def input():
     line = sys.argv
@@ -20,23 +20,26 @@ def input():
 
     if len(line) == 4:
         max_iter = "200"
-    else: max_iter = line[2]
+    else:
+        max_iter = line[2]
     k, max_iter = validate_int(k, max_iter)
 
     file_name1 = line[-1]
     file_name2 = line[-2]
 
-    if not(k > 0 and max_iter > 0):
-        raise("One or more of the outputs were negative")
+    if not (k > 0 and max_iter > 0):
+        raise ("One or more of the outputs were negative")
 
     return (k, max_iter, file_name1, file_name2)
 
+
 def validate_int(k, max_iter):
     if not (k.isdigit()):
-        raise("k.isdigit exception")
+        raise ("k.isdigit exception")
     if not (max_iter.isdigit()):
-        raise("max_iter.isdigit exception")
+        raise ("max_iter.isdigit exception")
     return int(k), int(max_iter)
+
 
 def read_file(path):
     m = []
@@ -48,29 +51,32 @@ def read_file(path):
             if line != '':
                 vector = [float(x) for x in line.split(",")]
                 m.append(vector)
-            else: stop = True
+            else:
+                stop = True
         except EOFError:
             stop = True
     return m
 
+
 def load_data(p1, p2):
     m1 = np.array(read_file(p1))
     m2 = np.array(read_file(p2))
-    
+
     df1 = pd.DataFrame(m1, index=[m1[i][0] for i in range(len(m1))])
     df2 = pd.DataFrame(m2, index=[m2[i][0] for i in range(len(m2))])
-    
+
     df1 = df1.drop(0, axis=1)
     df2 = df2.drop(0, axis=1)
 
-    result = pd.concat([df2, df1], join="inner", axis=1 )
+    result = pd.concat([df2, df1], join="inner", axis=1)
     result = result.sort_index()
-    #print(result)
+    # print(result)
 
-    #result.columns = ["1","2","3","4"]
+    # result.columns = ["1","2","3","4"]
 
     d = result.to_numpy()
     return d
+
 
 def select_initial(datapoints, observations):
     np.random.seed(0)
@@ -79,13 +85,15 @@ def select_initial(datapoints, observations):
     observations[0] = rand_index
     return u1
 
+
 def build_probabilities(D):
     size = len(D)
-    probabilities = [0]*size
+    probabilities = [0] * size
     s = sum(D)
     for i in range(size):
-        probabilities[i] = D[i]/s
+        probabilities[i] = D[i] / s
     return probabilities
+
 
 def distance(u, v):
     res = 0
@@ -93,47 +101,61 @@ def distance(u, v):
         res += (u[i] - v[i]) ** 2
     return res
 
-def computeDi (data, xi, Z, centroids):
+
+def computeDi(data, xi, Z, centroids):
     Di = distance(xi, centroids[0])
-    for j in range(1,Z):
+    for j in range(1, Z):
         uj = centroids[j]
         d = distance(xi, uj)
         Di = min(d, Di)
     return Di
 
-def kmeanspp (datapoints, k):
-    initial_centroids = [0]*k
-    observations = [0]*k
+
+def kmeanspp(datapoints, k):
+    initial_centroids = [0] * k
+    observations = [0] * k
     initial_centroids[0] = select_initial(datapoints, observations)
     size = len(datapoints)
     Z = 1
-    while Z<k:
-        D = [[0]for el in range(size)]
+    while Z < k:
+        D = [[0] for el in range(size)]
         for i in range(size):
             xi = np.copy(datapoints[i])
             D[i] = computeDi(datapoints, xi, Z, initial_centroids)
         probabilities = build_probabilities(D)
-        j = np.random.choice(size,p=probabilities)
+        j = np.random.choice(size, p=probabilities)
         observations[Z] = j
         initial_centroids[Z] = np.copy(datapoints[j])
-        Z+=1
-    return (observations,[array.tolist() for array in initial_centroids])
+        Z += 1
+    return (observations, [array.tolist() for array in initial_centroids])
+
+
+def arr_to_str(arr):
+    lst = [str(x) for x in arr]
+    res = " ".join(lst)
+    return res
+
 
 def main():
     (k, max_iter, file_name1, file_name2) = input()
-    data = load_data(file_name1,file_name2)
+    data = load_data(file_name1, file_name2)
     data_len = len(data)
     vector_len = len(data[0])
     (initial_indexes, initial_vectors) = kmeanspp(data, k)
-    
+
     result = km.fit(k, max_iter, data_len, vector_len, initial_vectors, data.tolist())
 
     round_result = np.round(result, 4)
 
+    # print(initial_indexes)
+    # print(result)
     #print(initial_indexes)
-    #print(result)
+    #print(round_result)
 
-    print(initial_indexes)
-    print(round_result)
+    print(arr_to_str(initial_indexes))
+    for line in round_result:
+        print(arr_to_str(line))
+
+
 
 main()
